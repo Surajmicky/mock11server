fbd84d5ff15199873de8a880e4ee9a2ae882ec07
Suraj kumar <surajmicky10@gmail.com>
2023-01-19 13:32:44 +0530

    changed link

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,6 +2,6 @@ const mongoose= require('mongoose');
 mongoose.set('strictQuery', false)
 module.exports= function connect(){
 
-    return mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+    return mongoose.connect('mongodb+srv://suraj:suraj123@cluster0.dugw8ja.mongodb.net/mock11?retryWrites=true&w=majority', { useNewUrlParser: true })
 }
 //'mongodb+srv://suraj:suraj123@cluster0.dugw8ja.mongodb.net/mock11?retryWrites=true&w=majority'
