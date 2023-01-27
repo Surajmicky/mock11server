84907d6198be673c97a6d00e9fa5fe7a9cb78a5b
Suraj kumar <surajmicky10@gmail.com>
2023-01-27 15:12:21 +0530

    added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,20 @@
 
 const mongoose= require('mongoose')
 const userSchema= new mongoose.Schema({
-    email:{
+    name:{
+        type:String,
+        required:true
+    },email:{
         type:String,
         required:true
     },password:{
+        type:String,
+        required:true
+    },username:{
+        type:String,
+        required:true
+    },
+    description:{
         type:String,
         required:true
     }
